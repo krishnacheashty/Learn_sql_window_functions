3279077007823bba1ac4a855b825b689a02d706d
krishna cheashty <krishnacheashty@gmail.com>
2026-02-27 01:29:42 +0600

    learn about window function

---
+++ WINDOWS_FUNCTIONS.sql
@@ -0,0 +1,59 @@
+--               task-01
+--find the number of customers
+--FIND the total sales of all orders
+--Find total average sales of all orders
+--find the highest sales of all orders
+--Find the lowest sales of all orders
+
+SELECT 
+c.CustomerID,
+COUNT(*) AS TOTAL_NU_SALES,
+SUM(C.Score) Total_sales,
+AVG(C.Score) AS average_sales,
+MAX(C.Score) AS max_sales,
+MIN(C.Score) AS Min_sales
+FROM Sales.Customers AS C
+GROUP BY CustomerID;
+
+
+					/*	WINDOWS FUNCTIONS   */
+
+/*What is windows functions ?
+
+perfrom calculations(e.g aggregation) on a specific subset of data with 
+losing the level of details of rows.
+
+*/
+
+--TOTAL SALES FOR EACH PRODUCT
+
+SELECT 
+ProductID,
+--CustomerID,
+--OrderID
+SUM(Sales)
+FROM Sales.Orders
+GROUP BY ProductID
+
+--group by changes the granularity
+/* windows functions
+
+aggregate function
+1.count()	2.sum()	3.avg()		4.MIN()		5.MIN()
+
+RANK functions 
+
+1.ROW_NUMBER()
+2.RANK()
+3.DENSE_RANK()
+4.CUME_DIST()
+5.PERCENT_RANK()
+6.NTIIE(n)
+
+VALUE (Analytics) function
+
+1.LEAD(expr,offset,default)
+2.LAG(expr,offset,default)
+3.FIRST_VALUE(expr)
+4.LAST_VALUE(expr)
+*/
