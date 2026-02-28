f6eb0593e2c3efb3e45b1a63e9dc28739b4057f8
krishna cheashty <krishnacheashty@gmail.com>
2026-03-01 01:26:38 +0600

    ranking function details

---
+++ window_ranking_function.sql
@@ -0,0 +1,23 @@
+SELECT 
+*
+FROM Sales.Orders
+
+--					RANKING WINDOW FUNCTION
+/*  1) ROW_NUMBER()--Assign a unique number to each in a window.
+				syntax-- ROW_NUMBER () OVER(ORDER BY _____)
+	2) RANK() -- Assign a rank to each row in a window,with gaps.
+				syntax-- RANK () OVER(ORDER BY _____)
+	3) DENSE_RANK() --Assign a rank to each row in a window, without gaps.
+				syntax-- DENSE_RAMK () OVER(ORDER BY _____)
+	4)CUME_DIST()-- calculates the cumulative distribution of a value
+					within a set of values.
+				syntax-- CUME_DIST () OVER(ORDER BY _____)
+	5)PERCENT_RANK () --Returns the percentile ranking number of 
+						approximately equal group.
+				syntax-- PERSENT_RANK () OVER(ORDER BY _____)
+	6)NTILE ()--Divides the rows into a specified number of approximately
+				equal group.
+				syntax-- NTILE (n) OVER(ORDER BY _____)
+
+				here n is 1,2,3,4,5.....
+				*/
